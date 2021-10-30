c2983577dcac55472b41785cfd96d1fe742bd135
MICHAEL RODILES <84428163+mrodiles@users.noreply.github.com>
2021-10-30 17:42:10 -0400

    routes name change (#16)
    * CookBook model added
    
    * userToken Export
    
    * cookbook-routes
    
    * routes name change

--- server/routes/cookBook.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-// const CookBook = require("../models/CookBook.model");
-// const Recipe = require("../models/Recipe.model");
-
-router.get("/my-cookbooks", (req, res) => {
-  res.send("This is my cookBook!");
-});
-
-module.exports = router;
