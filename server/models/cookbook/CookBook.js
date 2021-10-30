697364bb628c0280cdae3f114f0a757306f7321b
kpla001 <kpla001@fiu.edu>
2021-10-30 17:03:19 -0400

    add preroutes to app.js

--- server/models/cookbook/CookBook.js
+++ server/models/cookbook/CookBook.js
@@ -15,19 +15,10 @@ const cookBookSchema = new Schema({
     ref: "User",
   },
 
-<<<<<<< HEAD
   recipes: [{
     type: Schema.Types.ObjectId,
     ref: "Recipe"
   }],
-=======
-  recipes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "recipe",
-    },
-  ],
->>>>>>> 4f7022d3d0c2ce942a7fce76fff2dfabdf46f893
 
   image: String,
 
