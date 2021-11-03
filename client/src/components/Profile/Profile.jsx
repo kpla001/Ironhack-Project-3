4435ecce7bd7909bd79a1df00469f37573e33df7
Rodrigo <rgonzalez09@gmail.com>
2021-11-03 19:10:39 -0400

    view recipe on profile page

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import './Profile.css';
-import service from '../../services/service';
-import Ingredients from '../Ingredients/Ingredients';
+import React, { Component } from "react";
+import "./Profile.css";
+import service from "../../services/service";
+import Ingredients from "../Ingredients/Ingredients";
+import Recipe from "../Recipe/Recipe";
 
 export default class Profile extends Component {
-    state = {
-        ingredients: [],
-    };
+  state = {
+    ingredients: [],
+    recipes: [],
+  };
 
-    
+  componentDidMount() {
+    service.getIngredientsList().then((data) => {
+      this.setState({
+        ingredients: data.ingredients,
+      });
+    });
+    service.getRecipeList().then((data) => {
+      this.setState({
+        recipes: data.recipes,
+      });
+    });
+  }
 
-    componentDidMount(){
-        service.getIngredientsList().then((data) => {
-            this.setState({
-                ingredients: data.ingredients,
-            });
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <Ingredients ingredients={this.state.ingredients}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Ingredients ingredients={this.state.ingredients} />
+        <Recipe recipes={this.state.recipes} />
+      </div>
+    );
+  }
 }
---
+++ client/src/components/Recipe/Recipe.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Recipe.css";
+import { Link } from "react-router-dom";
+
+export default function Recipe({ recipes }) {
+  return (
+    <div>
+      <div className="recipe-list">
+        {recipes.map((recipe) => (
+          <div key={recipe._id}>
+            <Link to={`recipe/${recipe._id}`}>{recipe.name}</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
