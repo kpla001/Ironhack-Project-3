873a1376d4215dff280bc11fb19f7c1fb3de8a94
kpla001 <kpla001@fiu.edu>
2021-11-03 18:19:06 -0400

    create Home and Search components, change /protected route to /profile

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                This is the homepage
+            </div>
+        )
+    }
+}
