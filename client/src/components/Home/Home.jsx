5afad0e75cdd75b01a8e200358e04459b0e4af77
kpla001 <kpla001@fiu.edu>
2021-11-04 21:42:45 -0400

    search bar and home page functionality

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import Search from '../Search/Search';
 
 export default class Home extends Component {
-    state = {
-        searchResults: '',
-    }
 
-    searchHandler = (input) => this.setState({
-        searchResults: input
-    })
+    
 
     render() {
-        console.log("search submitted:", this.state.searchResults.input)
+        
         return (
             <div>
                 This is the homepage
                 
-                <Search submitSearch={this.searchHandler} />
+                
             </div>
         )
     }
