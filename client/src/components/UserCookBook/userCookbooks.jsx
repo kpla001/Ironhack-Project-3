439b0704274158e893b6a8b6ba62c0c1e25a9790
MICHAEL RODILES <84428163+mrodiles@users.noreply.github.com>
2021-11-11 11:54:33 -0500

    Mrbranch (#63)
    * CookBook model added
    
    * userToken Export
    
    * cookbook-routes
    
    * routes name change
    
    * route fix
    
    * mod
    
    * routes&models
    
    * Cookbooks Routes Done
    
    * Cookbook Post
    
    * Minor Change
    
    * UserCookbook
    
    * Drop Down Cookbook/Recipes/Favorites
    
    * Profile Drop Down Recovered

--- client/src/components/UserCookBook/userCookbooks.jsx
+++ client/src/components/UserCookBook/userCookbooks.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import Like from "../common/like";
+import ListGroup from "../common/listGroup";
 import _ from "lodash";
 import Pagination from "../common/pagination";
 import { getCookbooks } from "../../services/fakeCookbookService.js";
+import { getRecipes } from "../../services/fakeRecipeService";
 import { paginate } from "../../utils/paginate";
 // import MoviesTable from "./moviesTable";
-// import ListGroup from "../common/listGroup";
 
 class UserCookbooks extends React.Component {
   state = {
-    cookbooks: getCookbooks(),
+    cookbooks: [],
+    recipes: [],
     currentPage: 1,
     pageSize: 4,
   };
+
+  componentDidMount() {
+    this.setState({ cookbooks: getCookbooks(), recipes: getRecipes() });
+  }
 
   handleDelete = (book) => {
     const cookbooks = this.state.cookbooks.filter((b) => b._id !== book._id);
@@ -33,61 +39,84 @@ class UserCookbooks extends React.Component {
     this.setState({ currentPage: page });
   };
 
+  handleRecipeSelect = (recipe) => {
+    this.setState({ selectedRecipe: recipe });
+  };
+
   render() {
     const { length: count } = this.state.cookbooks;
-    const { pageSize, currentPage, cookbooks: allCookbooks } = this.state; // add allCookbooks
+    const {
+      pageSize,
+      currentPage,
+      selectedRecipe,
+      cookbooks: allCookbooks,
+    } = this.state; // add allCookbooks
 
     if (count === 0) return <p>Add some good stuff!</p>;
 
-    const cookbooks = paginate(allCookbooks, currentPage, pageSize);
+    const filtered = selectedRecipe
+      ? allCookbooks.filter((m) => m.recipe._id === selectedRecipe._id)
+      : allCookbooks;
+
+    const cookbooks = paginate(filtered, currentPage, pageSize);
 
     return (
-      <React.Fragment>
-        <p>Showing {count} books in the database.</p>
+      <div className="row">
+        <div className="col-3">
+          <ListGroup
+            items={this.state.recipes}
+            selectedItem={this.state.selectedRecipe}
+            onItemSelect={this.handleRecipeSelect}
+          />
+        </div>
+        <div className="col">
+          <p>Showing {filtered.length} books in the database.</p>
 
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Cookbooks</th>
-              <th>Recipes</th>
-              <th>Ingredients</th>
-              <th>Favorites</th>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {cookbooks.map((book) => (
-              <tr key={book._id}>
-                <td>{book.title}</td>
-                <td>{book.recipe.name}</td>
-                <td>{book.numberInStock}</td>
-                <td>{book.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    liked={book.liked}
-                    onClick={() => this.handleLike(book)}
-                  />
-                </td>
-                <td>
-                  <button
-                    onClick={() => this.handleDelete(book)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
-                </td>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Name</th>
+                <th>Ingredients</th>
+                <th>Favorite</th>
+                <th />
+                <th />
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {cookbooks.map((book) => (
+                <tr key={book._id}>
+                  <td>{book.title}</td>
+                  <td>{book.recipe.name}</td>
+                  <td>{book.numberInStock}</td>
+                  <td>{book.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={book.liked}
+                      onClick={() => this.handleLike(book)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDelete(book)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
         <Pagination
-          itemsCount={count}
+          itemsCount={filtered.length}
           pageSize={pageSize}
           currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
-      </React.Fragment>
+      </div>
     );
   }
 }
