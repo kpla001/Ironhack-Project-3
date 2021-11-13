322febb63d147309a1037a781a5fad6b1ed73e4c
“Michael <michaelrodiles@gmail.com>
2021-11-13 15:05:23 -0500

    pagination attempt

--- client/src/pages/DetailsPage.jsx
+++ client/src/pages/DetailsPage.jsx
@@ -1,31 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import RecipeDetails from "../components/RecipeDetails/RecipeDetails";
 import apiService from "../services/apiService";
-import axios from 'axios';
+import axios from "axios";
 
 export default class DetailsPage extends Component {
-    state={
-        recipe: null
-    }
+  state = {
+    recipe: null,
+    currentPage: 1,
+    pageSize: 4,
+  };
 
-    componentDidMount(){
-        apiService.getRecipeDetailsFromApi(this.props).then(recipeFromApi => {
-            // console.log(recipeFromApi.data)
-            this.setState({
-                recipe: recipeFromApi.data,
-            })
-        })
+  componentDidMount() {
+    apiService.getRecipeDetailsFromApi(this.props).then((recipeFromApi) => {
+      // console.log(recipeFromApi.data)
+      this.setState({
+        recipe: recipeFromApi.data,
+      });
+    });
+  }
 
-    }
+  handleRecipeSelect = (recipe) => {
+    this.setState({ selectedRecipe: recipe, currentPage: 1 });
+  };
 
-    render() {
-        // console.log(this.state?.recipe)
-        return (
-            <div>
-                <br/>
-                <RecipeDetails recipe={this.state.recipe} />
-            </div>
-        )
-    }
+  render() {
+    // console.log(this.state?.recipe)
+    return (
+      <div>
+        <br />
+        <RecipeDetails recipe={this.state.recipe} />
+      </div>
+    );
+  }
 }
