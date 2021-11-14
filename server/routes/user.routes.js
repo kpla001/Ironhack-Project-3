f9d54fe3f063fe73ddc2bfd3877c9c7b21a35416
kpla001 <kpla001@fiu.edu>
2021-11-13 22:32:34 -0500

    edit logo

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,6 +6,19 @@ const Recipe = require("../models/recipe/Recipe");
 const Ingredient = require("../models/ingredient/Ingredient");
 const User = require("../models/user/User.model");
 
+router.get("/", (req, res) => {
+    User.find()
+    .populate('cookbooks')
+    .then(userData => {
+        // console.log(userData)
+        res.status(200).json({ user: userData });
+    })
+    .catch((err) => {
+        // console.log(err);
+        res.json({ errorMessage: err });
+    });
+})
+
 router.get("/:id", (req, res) => {
     User.findById(req.params.id)
     .populate('cookbooks')
