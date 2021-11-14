e25c59f360e3ce6146799a4bcded1aac74f9a8ed
MICHAEL RODILES <84428163+mrodiles@users.noreply.github.com>
2021-11-13 22:23:49 -0500

    cookBook User Data (#81)
    * CookBook model added
    
    * userToken Export
    
    * cookbook-routes
    
    * routes name change
    
    * route fix
    
    * mod
    
    * routes&models
    
    * Cookbooks Routes Done
    
    * Cookbook Post
    
    * Minor Change
    
    * UserCookbook
    
    * Drop Down Cookbook/Recipes/Favorites
    
    * Profile Drop Down Recovered
    
    * drop down fixed names
    
    * pagination attempt
    
    * CookBooks service call
    
    * userCookbook Data

--- client/src/components/UserCookBook/userCookbooks.jsx
+++ client/src/components/UserCookBook/userCookbooks.jsx
@@ -20,8 +20,10 @@ class UserCookbooks extends React.Component {
   };
 
   componentDidMount() {
-    service.getCookbookList().then((data) => {
-      this.setState({ cookbooks: data.cookbooks });
+    service.getUserCookBooksById(this.props.user._id).then((data) => {
+      // console.log("This is our data ------------------------", data);
+      // console.log("Props: ====================", this.props);
+      this.setState({ cookbooks: data.user.cookbooks });
     });
 
     // const recipes = [{ _id: "", name: "All My Resepez" }, ...getRecipes()];
@@ -29,28 +31,28 @@ class UserCookbooks extends React.Component {
     // this.setState({ cookbooks: getCookbooks(), recipes });
   }
 
-  handleDelete = (book) => {
-    const cookbooks = this.state.cookbooks.filter((b) => b._id !== book._id);
-    this.setState({ cookbooks: cookbooks });
-  };
+  // handleDelete = (book) => {
+  //   const cookbooks = this.state.cookbooks.filter((b) => b._id !== book._id);
+  //   this.setState({ cookbooks: cookbooks });
+  // };
 
-  handleLike = (book) => {
-    console.log("Like CLicked", book);
-    const cookbooks = [...this.state.cookbooks];
-    const index = cookbooks.indexOf(book);
-    cookbooks[index] = { ...cookbooks[index] };
-    cookbooks[index].liked = !cookbooks[index].liked;
-    this.setState({ cookbooks });
-  };
+  // handleLike = (book) => {
+  //   console.log("Like CLicked", book);
+  //   const cookbooks = [...this.state.cookbooks];
+  //   const index = cookbooks.indexOf(book);
+  //   cookbooks[index] = { ...cookbooks[index] };
+  //   cookbooks[index].liked = !cookbooks[index].liked;
+  //   this.setState({ cookbooks });
+  // };
 
-  handlePageChange = (page) => {
-    // console.log(page);
-    this.setState({ currentPage: page });
-  };
+  // handlePageChange = (page) => {
 
-  handleRecipeSelect = (recipe) => {
-    this.setState({ selectedRecipe: recipe, currentPage: 1 });
-  };
+  //   this.setState({ currentPage: page });
+  // };
+
+  // handleRecipeSelect = (recipe) => {
+  //   this.setState({ selectedRecipe: recipe, currentPage: 1 });
+  // };
 
   // handleSort = (path) => {
   //   console.log(path);
@@ -58,54 +60,57 @@ class UserCookbooks extends React.Component {
   // };
 
   render() {
-    const { length: count } = this.state.cookbooks;
-    const {
-      pageSize,
-      currentPage,
-      // sortColumn,
-      selectedRecipe,
-      cookbooks: allCookbooks,
-    } = this.state; // add allCookbooks
+    console.log(this.state.cookbooks);
+    return <div>{this.state.cookbooks[0]?.title}</div>;
 
-    if (count === 0) return <p>Add some good stuff!</p>;
+    // const { length: count } = this.state.cookbooks;
+    // const {
+    //   pageSize,
+    //   currentPage,
 
-    const filtered =
-      selectedRecipe && selectedRecipe._id
-        ? allCookbooks.filter((m) => m.recipe._id === selectedRecipe._id)
-        : allCookbooks;
+    //   selectedRecipe,
+    //   cookbooks: allCookbooks,
+    // } = this.state;
+
+    // if (count === 0) return <p>Add some good stuff!</p>;
+
+    // const filtered =
+    //   selectedRecipe && selectedRecipe._id
+    //     ? allCookbooks.filter((m) => m.recipe._id === selectedRecipe._id)
+    //     : allCookbooks;
 
     // const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
-    const cookbooks = paginate(filtered, currentPage, pageSize);
+    // const cookbooks = paginate(filtered, currentPage, pageSize);
+    // console.log(this.props);
+    // return (
+    //   <div className="row">
+    //     <div className="col-3">
+    //       <ListGroup
+    //         items={this.state.recipes}
+    //         selectedItem={this.state.selectedRecipe}
+    //         onItemSelect={this.handleRecipeSelect}
+    //       />
+    //     </div>
+    //     <div className="col">
+    //       <p>Showing {filtered.length} items saved by user.</p>
 
-    return (
-      <div className="row">
-        <div className="col-3">
-          <ListGroup
-            items={this.state.recipes}
-            selectedItem={this.state.selectedRecipe}
-            onItemSelect={this.handleRecipeSelect}
-          />
-        </div>
-        <div className="col">
-          <p>Showing {filtered.length} items saved by user.</p>
+    //       <UserTable
+    //         cookbooks={cookbooks}
+    //         onLike={this.handleLike}
+    //         onDelete={this.handleDelete}
+    //         onSort={this.handleSort}
+    //       />
+    //     </div>
 
-          <UserTable
-            cookbooks={cookbooks}
-            onLike={this.handleLike}
-            onDelete={this.handleDelete}
-            onSort={this.handleSort} /// work in progress
-          />
-        </div>
-
-        <Pagination
-          itemsCount={filtered.length}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange}
-        />
-      </div>
-    );
+    //     <Pagination
+    //       itemsCount={filtered.length}
+    //       pageSize={pageSize}
+    //       currentPage={currentPage}
+    //       onPageChange={this.handlePageChange}
+    //     />
+    // </div>
+    // );
   }
 }
 
