b4a2601875f94d38d550c2368397178e2a82c127
MICHAEL RODILES <84428163+mrodiles@users.noreply.github.com>
2021-11-13 19:22:13 -0500

    Fixed merge (#76)
    * CookBook model added
    
    * userToken Export
    
    * cookbook-routes
    
    * routes name change
    
    * route fix
    
    * mod
    
    * routes&models
    
    * Cookbooks Routes Done
    
    * Cookbook Post
    
    * Minor Change
    
    * UserCookbook
    
    * Drop Down Cookbook/Recipes/Favorites
    
    * Profile Drop Down Recovered
    
    * drop down fixed names
    
    * pagination attempt

--- client/src/pages/DetailsPage.jsx
+++ client/src/pages/DetailsPage.jsx
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import RecipeDetails from "../components/RecipeDetails/RecipeDetails";
 import apiService from "../services/apiService";
-<<<<<<< HEAD
-import axios from "axios";
-=======
 import service from "../services/service";
->>>>>>> 80e1c11584f38015cc98bbee9a5e68eb4c2d85db
 
 export default class DetailsPage extends Component {
   state = {
@@ -28,58 +24,48 @@ export default class DetailsPage extends Component {
     this.setState({ selectedRecipe: recipe, currentPage: 1 });
   };
 
-<<<<<<< HEAD
+  saveRecipeToDb(recipe) {
+    // console.log("look here--------------", recipe)
+    const recipeData = {
+      // ...recipe,
+      name: recipe.title,
+      spoonacularId: recipe.id,
+      ingredients: recipe.extendedIngredients.map((ingredient) => {
+        return {
+          name: ingredient.name,
+          spoonacularId: `${ingredient.id ? ingredient.id : Date.now()}`,
+          image: ingredient.image,
+        };
+      }),
+      directions: recipe.analyzedInstructions,
+      image: recipe.image,
+      calories: recipe.nutrition.nutrients[0].amount,
+      cuisines: recipe.cuisines,
+      dairyFree: recipe.dairyFree,
+      dishTypes: recipe.dishTypes,
+      glutenFree: recipe.glutenFree,
+      readyInMinutes: recipe.readyInMinutes,
+      servings: recipe.servings,
+      vegan: recipe.vegan,
+      vegetarian: recipe.vegetarian,
+    };
+    console.log({ recipeData });
+    service.postRecipeToDb(recipeData);
+  }
+
   render() {
     // console.log(this.state?.recipe)
+    // console.log("props:",this.props)
     return (
       <div>
         <br />
         <RecipeDetails recipe={this.state.recipe} />
+        {this.props.user && (
+          <button onClick={() => this.saveRecipeToDb(this.state.recipe)}>
+            Save Recipe
+          </button>
+        )}
       </div>
     );
   }
-=======
-    saveRecipeToDb(recipe){
-        // console.log("look here--------------", recipe)
-        const recipeData = {
-            // ...recipe,
-            name: recipe.title,
-            spoonacularId: recipe.id,
-            ingredients: recipe.extendedIngredients.map((ingredient) => {
-                return { 
-                    name: ingredient.name,
-                    spoonacularId: `${ingredient.id ? ingredient.id : Date.now()}`,
-                    image: ingredient.image,
-                }
-            }),
-            directions: recipe.analyzedInstructions,
-            image: recipe.image,
-            calories: recipe.nutrition.nutrients[0].amount,
-            cuisines: recipe.cuisines,
-            dairyFree: recipe.dairyFree,
-            dishTypes: recipe.dishTypes,
-            glutenFree: recipe.glutenFree,
-            readyInMinutes: recipe.readyInMinutes,
-            servings: recipe.servings,
-            vegan: recipe.vegan,
-            vegetarian: recipe.vegetarian,
-
-        }
-        console.log({recipeData});
-        service.postRecipeToDb(recipeData)
-
-    }
-
-    render() {
-        // console.log(this.state?.recipe)
-        // console.log("props:",this.props)
-        return (
-            <div>
-                <br/>
-                <RecipeDetails recipe={this.state.recipe} />
-                {this.props.user && <button onClick={() => this.saveRecipeToDb(this.state.recipe)}>Save Recipe</button>}
-            </div>
-        )
-    }
->>>>>>> 80e1c11584f38015cc98bbee9a5e68eb4c2d85db
 }
