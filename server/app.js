d6ef966c117b654d4cddb2af12b8a7091c7a5c84
kpla001 <kpla001@fiu.edu>
2021-11-19 16:16:19 -0500

    add cors fix

--- server/app.js
+++ server/app.js
@@ -15,14 +15,7 @@ const app = express();
 require('./config')(app);
 
 // NEW - Add CORS headers - see https://enable-cors.org/server_expressjs.html
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
+
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,20 @@
-const router = require("express").Router();
-const authRoutes = require("./auth");
-const cookBookRoutes = require("./cookBook.routes");
-const recipeRoutes = require("./recipe.routes");
-const ingredientRoutes = require("./recipe.routes");
+const router = require('express').Router();
+const authRoutes = require('./auth');
+const cookBookRoutes = require('./cookBook.routes');
+const recipeRoutes = require('./recipe.routes');
+const ingredientRoutes = require('./recipe.routes');
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.get('/', (req, res, next) => {
+  res.json('All good in here');
 });
 
-router.use("/auth", authRoutes);
+router.use('/auth', authRoutes);
 
-router.use("/cookbooks", cookBookRoutes);
+router.use('/cookbooks', cookBookRoutes);
 
-router.use("/recipes", recipeRoutes);
+router.use('/recipes', recipeRoutes);
 
-router.use("/ingredients", ingredientRoutes);
+router.use('/ingredients', ingredientRoutes);
 
 module.exports = router;
