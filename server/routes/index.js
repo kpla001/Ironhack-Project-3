1836586b9dacfe38f6c2186cb1eb2d3a2f5440c7
kpla001 <kpla001@fiu.edu>
2021-11-19 13:19:44 -0500

    add recipes and ingredients routes to server/routes/index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
 const cookBookRoutes = require("./cookBook.routes");
+const recipeRoutes = require("./recipe.routes");
+const ingredientRoutes = require("./recipe.routes");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -10,5 +12,9 @@ router.get("/", (req, res, next) => {
 router.use("/auth", authRoutes);
 
 router.use("/cookbooks", cookBookRoutes);
+
+router.use("/recipes", recipeRoutes);
+
+router.use("/ingredients", ingredientRoutes);
 
 module.exports = router;
