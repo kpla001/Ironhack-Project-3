2d38190c33901d08a8ec0ecc363e24651eff3581
kpla001 <kpla001@fiu.edu>
2021-11-19 22:24:35 -0500

    (fix): replace client/services/service.js base url with process.env variable

--- client/src/components/SelectCookBook/SelectCookBookForm.jsx
+++ client/src/components/SelectCookBook/SelectCookBookForm.jsx
@@ -29,11 +29,12 @@ export default class SelectCookBookForm extends Component {
     render() {
         console.log(this.state)
         console.log(this.props.userCookBookData)
+        console.log(this.state.cookBooks)
         console.log(this.state.cookBooks[0])
         return (
         <>
             <select id="cookBooks" name="cookBookList" onChange={this.onChangeHandler} value={this.state.cookBooks[0]}>
-                {!!this.props.userCookBookData && this.props.userCookBookData.map((cookBook, i) => (
+                {!!this.props?.userCookBookData && this.props?.userCookBookData.map((cookBook, i) => (
                     <option 
                     key={i} 
                     value={cookBook?._id}
